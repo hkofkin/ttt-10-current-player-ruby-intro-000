4aa42d90b11eb56bc7cf3f3663f53d4aea49ae0a
Hannah Kofkin <hkofkin@gmail.com>
2020-02-24 17:31:40 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,8 +6,6 @@ end
 
 def turn_count(board)
   board.each do |turn|
-#    user_input = gets.strip
-    index = input_to_index(user_input)
     counter = 0
     if board[index] == "X" || board[index] == "O"
       counter += 1
